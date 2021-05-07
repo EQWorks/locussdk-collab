bcc472c782e7eabe7b00b8c85fa41274dc3b316c
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2021-05-07 10:57:28 -0400

    recipe - search available POI lists by name

---
+++ recipes/search_avail_poi_lists.py
@@ -0,0 +1,7 @@
+from locussdk import get_avail_poi_lists
+
+
+def search_avail_poi_lists(name):
+    global_lists = get_avail_poi_lists(list_type='global')
+
+    return global_lists[global_lists['name'].str.contains(name)]
