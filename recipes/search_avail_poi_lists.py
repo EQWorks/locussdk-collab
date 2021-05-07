d017109cdaeca7620755ef52557ce9a184ff9572
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2021-05-07 13:38:21 -0400

    recipe - search_avail_poi_lists() support pd.Series.str.contains arguments such as 'case'

--- recipes/search_avail_poi_lists.py
+++ recipes/search_avail_poi_lists.py
@@ -1,7 +1,7 @@
 from locussdk import get_avail_poi_lists
 
 
-def search_avail_poi_lists(name):
+def search_avail_poi_lists(name, **kwargs):
     global_lists = get_avail_poi_lists(list_type='global')
 
-    return global_lists[global_lists['name'].str.contains(name)]
+    return global_lists[global_lists['name'].str.contains(name, **kwargs)]
